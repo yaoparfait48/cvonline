44cba559f3bdcea50c24a0ac069b9b1000856e28
yaoparfait48 <yaoparfait48@gmail.com>
2023-05-09 13:01:10 +0000

    new route

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,8 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
+
+
+Route.get('/test', async ({ view }) => {
+  return view.render('welcome')
+})
